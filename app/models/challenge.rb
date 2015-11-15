64d4ead8266a9a7bfb2e8b97add61f84108184ed
Jean Hamant <jhamant@gmail.com>
2015-11-15 03:50:06 +0100

    added dependencies

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,3 +1,4 @@
 class Challenge < ActiveRecord::Base
   attr_accessible :category, :description, :name, :step_id, :total_time
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :challenge_id, :email, :first_name, :last_name
+  has_many :challenges
 end
